5600e0fae524dc3c7e625800fcdd91f4eb5a05ed
Banyudirga <saidrinto@gmail.com>
2026-06-22 01:17:59 +0700

    ganti project supabase

--- server/supabase.ts
+++ server/supabase.ts
@@ -5,8 +5,8 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
 // These environment variables need to be set in your production environment
-const supabaseUrl = process.env.SUPABASE_URL || 'https://nalxmoworcikparifjyu.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hbHhtb3dvcmNpa3Bhcmlmanl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNjU3NzYsImV4cCI6MjA2ODc0MTc3Nn0.g6DXOsRtuOftYCejvReMAUzyyeQv6HAYuqfizU4cFC8';
+const supabaseUrl = process.env.SUPABASE_URL || 'https://pbgfscevcxdnrwhzkhlx.supabase.co/rest/v1/';
+const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiZ2ZzY2V2Y3hkbnJ3aHpraGx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NTg3NTQsImV4cCI6MjA5NzUzNDc1NH0.Crthdv8w6JePKqZLZl9A1tTJJ_snaKvsXa0zcuPJgSI';
 
 
 // Create a single supabase client for interacting with your database
